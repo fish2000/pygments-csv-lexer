13f85503e61a0f521e2b2c45a561f3febc0c5b0d
Alexander Böhn <fish2000@gmail.com>
2019-03-11 16:14:42 -0400

    Nitpick additions to README.md and setup.py ... image of the lexer in the “pygmentize -L” output in “README.md” ... proper file headers for “setup.py” – interpreter, encoding,     license and project description docstring ... some other OCD tweaks undeserving of description

--- README.md
+++ README.md
@@ -1,8 +1,12 @@
 CSV Lexer for Pygments
 ===================
 
-![CSV lexer demonstration](https://imgur.com/g8TtkgW.png)
+<!-- https://imgur.com/VADHMMY.png -->
+
+![CSV lexer in “pygmentize -L” list](https://imgur.com/Xf3tRh0.png)
 
 A simple, custom [CSV lexer](http://pygments.org/docs/lexerdevelopment/) for the [Pygments syntax highlighter](http://pygments.org/).
+
+![CSV lexer demonstration](https://imgur.com/g8TtkgW.png)
 
 The code is originally based on [this StackOverflow answer](https://stackoverflow.com/a/25508711/298171). The project, in its current incarnation, is a fork of [this repo here](https://github.com/Kristinita/PygmentsCSVLexer).
--- setup.py
+++ setup.py
@@ -1,10 +1,37 @@
-""" Install custom csv lexer
-    
-    Instructions:
-    
-    * http://www.iamjonas.me/2013/03/custom-syntax-in-pygments.html
-    * https://stackoverflow.com/a/39540683/298171
-"""
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# 
+#    pygments-csv-lexer – A simple CSV lexer for the Pygments syntax highlighter
+#    
+#    Copyright © 2019 Alexander Böhn
+#    
+#    Permission is hereby granted, free of charge, to any person obtaining a copy 
+#    of this software and associated documentation files (the "Software"), to deal 
+#    in the Software without restriction, including without limitation the rights 
+#    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell 
+#    copies of the Software, and to permit persons to whom the Software is 
+#    furnished to do so, subject to the following conditions:
+#    
+#    The above copyright notice and this permission notice shall be included in all 
+#    copies or substantial portions of the Software.
+#    
+#    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+#    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+#    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+#    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+#    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+#    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE 
+#    SOFTWARE.
+# 
+#   Instructions:
+# 
+#   * http://www.iamjonas.me/2013/03/custom-syntax-in-pygments.html
+#   * https://stackoverflow.com/a/39540683/298171
+# 
+""" pygments-csv-lexer – A simple CSV lexer for the Pygments syntax highlighter """
+
+from __future__ import print_function
+
 import sys, os
 from setuptools import find_packages, setup
 
@@ -100,17 +127,17 @@ setup(
     long_description=LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
     
-    keywords=" ".join(KEYWORDS),
     url=PROJECT_GH_URL, download_url=PROJECT_DL_URL,
     license=LICENSE, platforms=['any'],
+    
+    install_requires=INSTALL_REQUIRES,
+    keywords=" ".join(KEYWORDS),
     classifiers=CLASSIFIERS,
+    zip_safe=True,
     
     packages=find_packages(),
     package_data={ '' : ['*.*'] },
     include_package_data=True,
-    zip_safe=True,
-    
-    install_requires=INSTALL_REQUIRES,
     entry_points="""
     [pygments.lexers]
     csv = csvlexer.csv:CsvLexer
