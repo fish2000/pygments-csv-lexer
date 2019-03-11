309c28f58d05a222bc5502cfd554ec926161fd36
Alexander Böhn <fish2000@gmail.com>
2019-03-11 17:22:38 -0400

    Added inline test using Terminal256Formatter

--- csvlexer/csv.py
+++ csvlexer/csv.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 # @Author: SashaChernykh
 # @Editor: fish2000
 # @Date: 2018-06-04 08:01:23
@@ -49,9 +50,56 @@ class CsvLexer(RegexLexer):
             (r'(,)([^,\n]*)',   bygroups(Punctuation, Name.Constant),           'fifth'),
         ],
         'fifth': [
+            (r'(,)([^,\n]*)',   bygroups(Punctuation, Literal.String.Single),   'sixth'),
+        ],
+        'sixth': [
+            (r'(,)([^,\n]*)',   bygroups(Punctuation, Literal.Number),          'seventh'),
+        ],
+        'seventh': [
+            (r'(,)([^,\n]*)',   bygroups(Punctuation, Keyword),                 'eighth'),
+        ],
+        'eighth': [
+            (r'(,)([^,\n]*)',   bygroups(Punctuation, Name.Constant),           'ninth'),
+        ],
+        'ninth': [
             (r'(,)([^,\n]*)',   bygroups(Punctuation, Literal.String.Single),   'unsupported'),
         ],
         'unsupported': [
-            (r'.+',             Punctuation),
+            (r'(.+)',           bygroups(Punctuation)),
         ],
     }
+
+sample_csv_material = """
+trailer,125
+header,125,11,session start,0,Wed Mar  6 11:04:43 2019, + 414 msec
+argument,1,0x0,sflags
+argument,2,0x0,am_success
+argument,3,0x0,am_failure
+subject,-1,root,wheel,root,wheel,0,100339,0,0.0.0.0
+return,success,0
+trailer,125
+header,125,11,session start,0,Wed Mar  6 18:23:18 2019, + 301 msec
+argument,1,0x0,sflags
+argument,2,0x0,am_success
+argument,3,0x0,am_failure
+subject,-1,root,wheel,root,wheel,0,100340,0,0.0.0.0
+return,success,0
+trailer,125
+header,153,11,user authentication,0,Wed Mar  6 18:24:36 2019, + 169 msec
+subject,-1,root,wheel,root,wheel,290,100000,0,0.0.0.0
+text,Touch ID authentication
+return,success,0
+"""
+
+def test():
+    from pygments.formatters import Terminal256Formatter
+    from pygments import highlight
+    
+    print(highlight(sample_csv_material, CsvLexer(), Terminal256Formatter()))
+
+if __name__ == '__main__':
+    import os, sys
+    if os.environ.get('TM_PYTHON'):
+        print("Inline test won’t work in TextMate, aborting…", file=sys.stderr)
+        sys.exit(1)
+    test()
